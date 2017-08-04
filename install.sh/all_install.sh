b46e875da921605c81b76eee3a1eb6d68e8f0a56
Guan Hua <tulingjiaoyu@gmail.com>
2017-08-04 16:59:10 +0800

    rm

--- install.sh/all_install.sh
+++ install.sh/all_install.sh
@@ -2,18 +2,13 @@
 # Author: GH1995
 
 source ./install_software.sh
+source ./uninstall.sh
+
 # ------------------------------------------------------------+
 # 可选的一些工具和配置                                        |
 #                                                             |
-# fish                                                        |
-# The user-friendly command line shell. http://fishshell.com  |
-# sudo apt install fish                                       |
-#                                                             |
 # 中文文档[已过时]                                            |
 # sudo apt install manpages-zh -y                             |
-#                                                             |
-# Vim的配置                                                   |
-# sh <(curl https://j.mp/spf13-vim3 -L)                       |
 #                                                             |
 # spacemacs的配置                                             |
 # git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d  |
