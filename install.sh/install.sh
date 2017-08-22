39b366892811b84a52a276a546c27eab05bc472f
Guan Hua <tulingjiaoyu@gmail.com>
2017-08-22 20:52:36 +0800

    add 300

--- install.sh/install.sh
+++ install.sh/install.sh
@@ -7,7 +7,7 @@ echo "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security main rest
 sudo apt update
 
 # esstenial software, such as, zsh tmux
-sudo apt-get install build-essential cmake vim python-dev python3-dev zsh git tmux autojump emacs ctags g++ clang aria2 axel openssh-server openssh-client mysql-server mysql-client texlive-full fontforge latex-cjk-chinese pandoc
+sudo apt-get install build-essential cmake vim python-dev python3-dev zsh git tmux autojump emacs ctags g++ clang aria2 axel openssh-server openssh-client mysql-server mysql-client texlive-full fontforge latex-cjk-chinese pandoc python-autopep8
 
 # 卸载软件
 sudo apt purge xul-ext-ubufox rhythmbox
@@ -16,6 +16,9 @@ sudo apt purge libreoffice-*
 sudo apt purge unity-webapps-*
 sudo apt-get purge thunderbird
 sudo apt purge aisleriot transmission-common
+sudo apt purge libreoffice-common
+sudo apt purge unity-webapps-common
+sudo apt purge thunderbird totem rhythmbox empathy brasero simple-scan gnome-mahjongg aisleriot gnome-mines cheese gnome-sudoku transmission-common gnome-orca webbrowser-app landscape-client-ui-install  deja-dup empathy-common  gwibber gcalctool  gnome-power-manager  ubuntuone  bluez simple-scan hplip* printer-driver*  gedit mahjongg  gnome-sudoku gnomine wodim imagemagick*
 
 # 收尾工作
 sudo apt upgrade
